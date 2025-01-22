3881c7de6b1261716dee53b57b3a6731761c55cb
abdelrahman hossam <85772638+abdelrahman-hosam@users.noreply.github.com>
2025-01-22 20:25:03 +0200

    Update databaseConnection.js

--- blog_post/databaseConnection.js
+++ blog_post/databaseConnection.js
@@ -4,7 +4,12 @@ const { MongoClient } = require("mongodb")
 let dbconnection
 module.exports = {
     connectToDb: (cb) => {
-        MongoClient.connect('mongodb+srv://abdelrahmanhossam4532:93AvOvjOaTV13dLV@cluster0.jyr2n.mongodb.net/blog_POST?retryWrites=true&w=majority&appName=Cluster0')
+        const uri = 'mongodb+srv://abdelrahmanhossam4532:93AvOvjOaTV13dLV@cluster0.jyr2n.mongodb.net/blog_POST?retryWrites=true&w=majority&appName=Cluster0'
+        MongoClient.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            ssl: true,
+          })
         .then((client) => {
            dbconnection = client.db()
            return cb()
